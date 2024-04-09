783939dd3f2faaf965741ea64d3d0d04b94763d1
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-04-09 16:16:27 +0700

    add: operators

---
+++ 04_operators/go.mod
@@ -0,0 +1,3 @@
+module github.com/Celesca/operators
+
+go 1.21.5
---
+++ 04_operators/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 10
+	var b int = 20
+
+	fmt.Println(a + b)
+	fmt.Println(a - b)
+	fmt.Println(a * b)
+	fmt.Println(a / b)
+	fmt.Println(a % b)
+
+	fmt.Println(a == b)
+	fmt.Println(a != b)
+	fmt.Println(a > b)
+	fmt.Println(a < b)
+
+}
