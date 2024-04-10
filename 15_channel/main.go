ab99dbcfbf7fc322340ca526d81f90b337280ddd
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-04-11 01:41:27 +0700

    add: 15-channel 2

--- 15_channel/main.go
+++ 15_channel/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 // Channel เอาไว้ใช้กับ Goroutines และเอาไว้เก็บส่งข้อมูลได้
 // ขั้นสูงกว่า wait โดยปกติมันไม่รู้ว่าจะต้องจบตอนไหน รับค่าตอนไหน
@@ -13,19 +16,27 @@ import "fmt"
 // Go wait ไม่รู้จะจบตอนไหน แต่ถ้ามันครบรอบแล้ว มันก็จะหยุด
 // function use case ที่มีการรับส่งข้อมูล ควรใช้ channel แทน wait
 
+// ถ้ารับ buffer ไม่ครบ จะเกิด deadlock รอตลอดไป
+
 func main() {
 
 	jobCh := make(chan int, 10)
 	resultCh := make(chan int, 10)
 
-	for i := range 10 {
+	for i := range []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9} {
 		jobCh <- i + 1
 	}
 
 	close(jobCh) // กัน deadlock ทุกครั้งที่มี assign แล้วต้อง close ไม่รับค่าแล้ว ครบ buffered
 
-	double(jobCh, resultCh)
-	for range 10 {
+	// สร้าง Goroutines 2 ตัว แยกร่างทำงานสองอันพร้อมกัน ch ตัวไหนยังไม่ได้ทำงาน มันก็จะแยกทำงานคู่ขนานกัน
+
+	// Worker Pull -> กันพวก Concurrency Max
+	for range []int{0, 1} {
+		go double(jobCh, resultCh)
+	}
+
+	for range []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9} {
 		result := <-resultCh
 		fmt.Println(result)
 	}
@@ -35,6 +46,9 @@ func main() {
 
 func double(jobCh <-chan int, resultCh chan<- int) {
 	for j := range jobCh {
+		time.Sleep(1 * time.Second)
 		resultCh <- j * 2
 	}
 }
+
+// ถ้าเราประยุกต์ใช้ ใช้กับฟังก์ชัน Download Upload ทีละหลายไฟล์
